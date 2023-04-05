0d0216f868091aaeeccf5703d91388e3d1002e9c
Jesse Thomas <jgthomas3@gmail.com>
2023-04-05 09:57:04 -0600

    Readd passenger_spec file because it reverted back to unfinished one, sweet passenger.rb

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,31 +1,28 @@
 require 'rspec'
 require './lib/passenger'
 
-RSpec.descibe Passenger do
+RSpec.describe Passenger do
 
   before(:each) do 
 
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
-    # => #<Passenger:0x00007fc1ad88b3c0...>
     
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
-    # => #<Passenger:0x00007fe0da2cf1b0...>
   end
 
   describe "initialize" do 
     
     it "exists" do 
       expect(@charlie).to be_an_instance_of(Passenger)
-      expect(@taylor.to be_a(Passenger))
+      expect(@taylor).to be_a(Passenger)
     end
 
     it "has readable attributes" do 
 
       expect(@charlie.name).to eq("Charlie")
-      # => "Charlie"
       
       expect(@charlie.age).to eq(18)
-      # => 18
+
       expect(@charlie.driver).to be(false)
     end
   end
@@ -35,21 +32,18 @@ RSpec.descibe Passenger do
     it "can check if person is an adult" do 
       
       expect(@charlie.adult?).to be(true)
-      # => true
       
       expect(@taylor.adult?).to be(false)
-      # => false
+
     end
   
     it "can make someone a driver if they're old enough" do 
 
-      expect(charlie.driver?).to be(false)
-      # => false
+      expect(@charlie.driver?).to be(false)
       
-      charlie.drive
+      @charlie.drive
       
-      expect(charlie.driver?).to be(true)
-      # => true
+      expect(@charlie.driver?).to be(true)
       
     end
   end
