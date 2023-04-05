2e7e37c40fc9200a28bda17fa903fab20751af6b
Jesse Thomas <jgthomas3@gmail.com>
2023-04-05 10:40:19 -0600

    Update num_adult test to assert properly and clean up comments passenger_spec.rb

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,19 +1,13 @@
 require './lib/vehicle'
 require './lib/passenger'
 
-RSpec.descibe Vehicle do 
+RSpec.describe Vehicle do 
   before(:each) do
 
     @vehicle = Vehicle.new("2001", "Honda", "Civic")    
-    # => #<Vehicle:0x00007fe0da9c63d8...>
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
-    # => #<Passenger:0x00007fe0da1ec450...>
-    
     @jude = Passenger.new({"name" => "Jude", "age" => 20})    
-    # => #<Passenger:0x00007fe0da2730e0...>
-    
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
-    # => #<Passenger:0x00007fe0da2cf1b0...>
     
   end
 
@@ -23,21 +17,18 @@ RSpec.descibe Vehicle do
       expect(@vehicle).to be_a(Vehicle)
     end
 
-    it "has readable attribute" do 
+    it "has readable attributes" do 
 
       expect(@vehicle.year).to eq("2001")
-      # => "2001"
       
       expect(@vehicle.make).to eq("Honda")
-      # => "Honda"
       
       expect(@vehicle.model).to eq("Civic")
-      # => "Civic"
 
       expect(@vehicle.passengers).to eq([])
-      # => []
       
-      expect(@vehicle.speed).to eq(0)
+      expect(@vehicle.speeding).to be(false)
+
     end
   end
 
@@ -46,52 +37,31 @@ RSpec.descibe Vehicle do
     it "#speeding?" do
     
     expect(@vehicle.speeding?).to be(false)
-    # => false
     
     @vehicle.speed
     
     expect(@vehicle.speeding?).to be(true)
-    # => true
-   end
 
-   #add check speed limit method? speed_limit(speed_limit) if speed is over speed limit = speeding
-    
+  end
 
-    # expect(@vehicle.passengers).to 
-    # # => []
-    
-    # charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
-    # # => #<Passenger:0x00007fe0da1ec450...>
-    
-    # jude = Passenger.new({"name" => "Jude", "age" => 20})    
-    # # => #<Passenger:0x00007fe0da2730e0...>
-    
-    # taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
-    # # => #<Passenger:0x00007fe0da2cf1b0...>
-    
     it "#add_passengers" do 
 
       @vehicle.add_passenger(@charlie)
-      
       @vehicle.add_passenger(@jude)
-      
       @vehicle.add_passenger(@taylor)
       
-      expect(@vehicle.passengers).to
-      # => [#<Passenger:0x00007fe0da1ec450...>, #<Passenger:0x00007fe0da2730e0...>, #<Passenger:0x00007fe0da2cf1b0...>]
+      expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
+
     end
     
     it "#num_adults" do
 
-    @vehicle.num_adults
-    # => 2
-  end
+      @vehicle.add_passenger(@charlie)
+      @vehicle.add_passenger(@jude)
+    
+      expect(@vehicle.num_adults).to eq(2)
     
     
+    end 
   end
-
-  
-
-
-
 end
