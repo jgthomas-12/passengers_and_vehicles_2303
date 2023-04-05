b9a567443eaa31bae77deeca58c126f46bc3a612
Jesse Thomas <jgthomas3@gmail.com>
2023-04-05 10:09:03 -0600

    Write tests from the interaction pattern vehicle_spec.rb

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -19,47 +19,78 @@ RSpec.descibe Vehicle do
 
   describe "initialize" do 
 
-    @vehicle.year
-    # => "2001"
+    it "exists" do 
+      expect(@vehicle).to be_a(Vehicle)
+    end
+
+    it "has readable attribute" do 
+
+      expect(@vehicle.year).to eq("2001")
+      # => "2001"
+      
+      expect(@vehicle.make).to eq("Honda")
+      # => "Honda"
+      
+      expect(@vehicle.model).to eq("Civic")
+      # => "Civic"
+
+      expect(@vehicle.passengers).to eq([])
+      # => []
+      
+      expect(@vehicle.speed).to eq(0)
+    end
+  end
+
+  describe "other methods" do 
+
+    it "#speeding?" do
     
-    @vehicle.make
-    # => "Honda"
+    expect(@vehicle.speeding?).to be(false)
+    # => false
     
-    @vehicle.model
-    # => "Civic"
+    @vehicle.speed
+    
+    expect(@vehicle.speeding?).to be(true)
+    # => true
+   end
+
+   #add check speed limit method? speed_limit(speed_limit) if speed is over speed limit = speeding
+    
+
+    # expect(@vehicle.passengers).to 
+    # # => []
+    
+    # charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    # # => #<Passenger:0x00007fe0da1ec450...>
+    
+    # jude = Passenger.new({"name" => "Jude", "age" => 20})    
+    # # => #<Passenger:0x00007fe0da2730e0...>
+    
+    # taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
+    # # => #<Passenger:0x00007fe0da2cf1b0...>
+    
+    it "#add_passengers" do 
+
+      @vehicle.add_passenger(@charlie)
+      
+      @vehicle.add_passenger(@jude)
+      
+      @vehicle.add_passenger(@taylor)
+      
+      expect(@vehicle.passengers).to
+      # => [#<Passenger:0x00007fe0da1ec450...>, #<Passenger:0x00007fe0da2730e0...>, #<Passenger:0x00007fe0da2cf1b0...>]
+    end
+    
+    it "#num_adults" do
+
+    @vehicle.num_adults
+    # => 2
+  end
+    
     
   end
-  @vehicle.speeding?
-  # => false
+
   
-  @vehicle.speed
-  
-  @vehicle.speeding?
-  # => true
-  
-  @vehicle.passengers
-  # => []
-  
-  # charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
-  # # => #<Passenger:0x00007fe0da1ec450...>
-  
-  # jude = Passenger.new({"name" => "Jude", "age" => 20})    
-  # # => #<Passenger:0x00007fe0da2730e0...>
-  
-  # taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
-  # # => #<Passenger:0x00007fe0da2cf1b0...>
-  
-  @vehicle.add_passenger(@charlie)    
-  
-  @vehicle.add_passenger(@jude)    
-  
-  @vehicle.add_passenger(@taylor)    
-  
-  @vehicle.passengers
-  # => [#<Passenger:0x00007fe0da1ec450...>, #<Passenger:0x00007fe0da2730e0...>, #<Passenger:0x00007fe0da2cf1b0...>]
-  
-  @vehicle.num_adults
-  # => 2
 
 
 
